f79515f20cee11b70e117e9fc567405e7e1ac96b
Ian McCowan <ian@bonanza.com>
2018-02-13 13:03:32 -0800

    initial commit

---
+++ config.el
@@ -0,0 +1,12 @@
+;; Use 2-space indent everywhere
+(setq-default standard-indent 2)
+
+;; Cache huge project
+(setq projectile-enable-caching t)
+
+(defun my-ruby-config ()
+  ;; Don't tell me about trailing whitespace
+  (setq show-trailing-whitespace nil))
+
+(add-hook 'enh-ruby-mode-hook 'my-ruby-config)
+(add-hook 'ruby-mode-hook 'my-ruby-config)
