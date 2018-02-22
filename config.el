7393bd30c2ca796f7de90c9257ddffc18f8eac32
Ian McCowan <ian@bonanza.com>
2018-02-22 12:42:18 -0800

    Turn off auto eol newlines

--- config.el
+++ config.el
@@ -6,7 +6,9 @@
 
 (defun my-ruby-config ()
   ;; Don't tell me about trailing whitespace
-  (setq show-trailing-whitespace nil))
+  (setq show-trailing-whitespace nil)
+  ;; Don't add newlines to the end of every file
+  (setq mode-require-final-newline nil))
 
 (add-hook 'enh-ruby-mode-hook 'my-ruby-config)
 (add-hook 'ruby-mode-hook 'my-ruby-config)
